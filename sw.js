1eb3b9b09ba233e5b115524b5d8d40e991f548b3
bidangrrjember <bidangrrjember@gmail.com>
2026-09-21 12:42:27 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
    lapangan tidak terus memakai versi lama yang tersimpan.
    ========================================================================= */
 
-const VERSI = "v9";
+const VERSI = "v11";
 const CACHE_APP = "jitupasna-app-" + VERSI;
 const CACHE_UBIN = "jitupasna-ubin";     // sengaja tanpa versi: ubin peta mahal
                                           // diunduh, jangan ikut terhapus tiap rilis
